c74998772cdb64f53269593e41dc6965cc250980
Anton <antonlashyk@gmail.com>
2020-02-11 17:08:49 +0300

    feat: add getLoveTrianglesCount func

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let count = 0;
+  for (let i = 0; i < preferences.length; i ++ ) {
+      let el1 = preferences[i] - 1;
+      if (el1 == i) continue;
+      let el2 = preferences[el1] - 1;
+      if (el2 == el1) continue;
+      let el3 = preferences[el2] - 1;
+      if (el3 == el2) continue;
+      if (el3 == i) {
+        count++;
+      }
+  }
+  return count/3;
 };
